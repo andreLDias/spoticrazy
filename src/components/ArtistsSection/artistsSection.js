c09761ff1e530ddbe5da95662ff4584713e4a5b3
Andre Dias <andreluizdiascustodio@gmail.com>
2022-06-15 10:51:19 -0300

    Add organization and main wireframe with poor design

--- src/components/ArtistsSection/artistsSection.js
+++ src/components/ArtistsSection/artistsSection.js
@@ -1,18 +1,30 @@
-import { ArtistImage, ArtistName, Wrapper } from './artistsSection.styles'
+import {
+  Artist,
+  ArtistImage,
+  ArtistName,
+  Wrapper,
+} from './artistsSection.styles'
 
 const ArtistsSection = ({ artists }) => {
   if (!artists.length) return
 
-  return artists.map((artist) => (
-    <Wrapper key={artist.id}>
-      <ArtistName>{artist.name}</ArtistName>
-      {artist.images.length ? (
-        <ArtistImage src={artist.images[0].url} alt={`${artist.name}-logo`} />
-      ) : (
-        <div>No Image</div>
-      )}
+  return (
+    <Wrapper>
+      {artists.map((artist) => (
+        <Artist key={artist.id}>
+          {artist.images.length ? (
+            <ArtistImage
+              src={artist.images[0].url}
+              alt={`${artist.name}-logo`}
+            />
+          ) : (
+            <div>No Image</div>
+          )}
+          <ArtistName>{artist.name}</ArtistName>
+        </Artist>
+      ))}
     </Wrapper>
-  ))
+  )
 }
 
 export { ArtistsSection }
